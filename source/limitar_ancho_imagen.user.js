8c38c0c930dad589ddc2c6fb01d827055af16c75
Daniel Gonzalez <mansonjesus@gmail.com>
2014-10-20 10:05:40 +0200

    Deleted important! mark

--- source/limitar_ancho_imagen.user.js
+++ source/limitar_ancho_imagen.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name       Limitar ancho de imagen
 // @namespace  http://mansonjesus.github.io/userscripts
-// @version    0.1
+// @version    0.2
 // @description  Limita el tamaño de imagen al 95% del ancho del bloque que la contiene
 // @match      http://www.forocoches.com/foro/showthread.php?*
 // @copyright  2014+, mansonjesus
 // ==/UserScript==
 
+// @TODO: Zoom imágenes al hacer click
 
 //Añade un estilo a la cabecera de la página 
 var style = document.createElement('style');
 style.type = 'text/css';
-style.innerHTML = 'img {max-width:95%;important!}';
+style.innerHTML = 'img {max-width:95%;}';
 document.getElementsByTagName('head')[0].appendChild(style);
